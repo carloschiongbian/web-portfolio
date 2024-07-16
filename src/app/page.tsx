026c9da03dcd095eeb5dbae411e979a3d5c08dc1
Carlos Julian H. Chiongbian <carlos.chiongbian.lanexcorp.com>
2024-07-16 18:42:46 +0800

    chore: removed unnused imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,7 @@
 "use client";
 
-import { LinkedinOutlined, MailOutlined } from "@ant-design/icons";
-import { Avatar, Col, Divider, Row, Switch, Timeline, Typography } from "antd";
-import { useEffect, useState } from "react";
-import Image from "next/image";
-import EducationTabs from "@/app/components/EducationTabs";
-import ProjectCard from "@/app/components/ProjectCard";
-import Home from "./pages/home/page";
+import { useEffect } from "react";
 import { useRouter } from "next/navigation";
-
-const { Title, Text } = Typography;
 
 export default function App() {
   const route = useRouter();
